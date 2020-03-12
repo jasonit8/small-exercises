254ed4f5f1dc4a2e697646c63091f52397462b01
Trọng Hiếu <jasonnfi8@gmail.com>
2020-03-12 22:06:44 +0700

    better function

--- python/is_this_number_prime.py
+++ python/is_this_number_prime.py
@@ -1,14 +1,2 @@
-def isprime(number):
-    if (type(number) != int) or (number < 2):
-        return False
-    elif number < 4:
-        return True
-    else:
-        primes = [2]
-        prime = 3
-        while prime <= number**.5:
-            primes += [prime] if all(prime % p > 0 for p in primes) else []
-            prime += 1
-        return all(number % p > 0 for p in primes)
-
+isprime = lambda number: all(number % i > 0 for i in range(2,int(number**.5)+1))
 # This program checks whether a number is prime or not.
