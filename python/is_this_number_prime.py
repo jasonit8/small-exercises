cf3310bf5ef5ecc54cd2bb5ef3856bb65d0a6fb8
Trọng Hiếu <jasonnfi8@gmail.com>
2020-03-12 20:20:16 +0700

    checks for prime number

---
+++ python/is_this_number_prime.py
@@ -0,0 +1,15 @@
+'''
+This program checks whether a number is prime or not.
+'''
+def isprime(number):
+    if (type(number) != int) or (number < 2):
+        return False
+    elif number < 4:
+        return True
+    else:
+        primes = [2]
+        prime = 3
+        while prime <= number**.5:
+            primes += [prime] if all(prime % p > 0 for p in primes) else []
+            prime += 1
+        return all(number % p > 0 for p in primes)
