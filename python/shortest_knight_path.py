b74cd9e1e5545ab92e78c191baed61c5e081e33f
Trọng Hiếu <jasonnfi8@gmail.com>
2020-03-12 23:09:01 +0700

    fixed string type

--- python/shortest_knight_path.py
+++ python/shortest_knight_path.py
@@ -1,6 +1,6 @@
 def knight(square1,square2):
     files = ['a','b','c','d','e','f','g','h']
-    ranks = [1,2,3,4,5,6,7,8]
+    ranks = [str(i) for i in range(1,9)]
     paths = {square1}
     steps = 0
     while square2 not in paths:
@@ -8,11 +8,11 @@ def knight(square1,square2):
         new_paths = set()
         for square in paths:
             file_index = files.index(square[0])
-            rank_index = ranks.index(int(square[1]))
+            rank_index = ranks.index(square[1])
             for i in [-2,-1,1,2]:
                 for j in [-2,-1,1,2]:
                     if i+j in [-3,-1,1,3]:
-                        new_paths.add(files[file_index + i] + str(ranks[rank_index + j])) if all([file_index + i in range(8),rank_index + j in range(8)]) else set()
+                        new_paths.add(files[file_index + i] + ranks[rank_index + j]) if all([file_index + i in range(8),rank_index + j in range(8)]) else set()
         paths = new_paths
     return steps
 
